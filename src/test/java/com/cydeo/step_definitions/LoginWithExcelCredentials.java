9fc1609550f5f8fdbe723c80c075e9b229a2283b
FExB17 <furkanervan@hotmail.de>
2023-03-10 10:52:35 +0100

    undone deleting cukes runner

--- src/test/java/com/cydeo/step_definitions/LoginWithExcelCredentials.java
+++ src/test/java/com/cydeo/step_definitions/LoginWithExcelCredentials.java
@@ -3,9 +3,7 @@ package com.cydeo.step_definitions;
 import com.cydeo.pages.BleucrmHomePage;
 import com.cydeo.pages.BleucrmLoginPage;
 import com.cydeo.utilities.Driver;
-import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -14,7 +12,7 @@ import org.junit.Assert;
 import java.io.FileInputStream;
 import java.io.IOException;
 
-public class Login_StepDefinitions {
+public class LoginWithExcelCredentials {
 
     BleucrmLoginPage loginPage = new BleucrmLoginPage();
     BleucrmHomePage homePage = new BleucrmHomePage();
@@ -34,23 +32,6 @@ public class Login_StepDefinitions {
     public void user_enters_valid(String string) {
         loginPage.usernameInput.sendKeys(string);
     }
-
-    // fo feature file with data table
-    @When("User enters valid password {string}")
-    public void user_enter_valid(String string) {
-        loginPage.passwordInput.sendKeys(string);
-    }
-
-    @When("User clicks login button")
-    public void user_clicks_login_button() {
-        loginPage.loginBtn.click();
-    }
-
-    @Then("User should be logged in")
-    public void user_should_be_logged_in() {
-        Assert.assertTrue(Driver.getDriver().getTitle().contains("Portal"));
-    }
-
 
     //for feature file with excel sheet
     @When("{string} User enters valid username and password")
